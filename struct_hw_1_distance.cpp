11292c189f63d7a28ef43cd3a6918eebcbd38857
Veritas <68550874+VERITASSSSSSS@users.noreply.github.com>
2021-08-02 16:16:58 +0800

    Update struct_hw_1_distance.cpp

--- struct_hw_1_distance.cpp
+++ struct_hw_1_distance.cpp
@@ -25,6 +25,37 @@ int main(){
     cout<<" P1 is set as(0,0). \n\n ";
     cout<<"Enter x and y for p2: ";cin>>a>>b;
     p2.set(a,b);
-    cout<<"\n"<<"P2 is on ("<<a<<", "<<b<<")\n\n"<<"You are "<<p1.distance(p2)<<" blocks away from the origin. \n"; //point ��0,0���Z��
+    cout<<"\n"<<"P2 is on ("<<a<<", "<<b<<")\n\n"<<"You are "<<p1.distance(p2)<<" blocks away from the origin. \n"; //point ¨ì0,0ªº¶ZÂ÷
     void print();
     }
+
+**********************************************************
+
+//structure --for app not for algorithms
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct point{ //point 定義
+    double x=0,y=0;
+    void set(double a, double b){ //function 接收外來變數
+        x=a;
+        y=b;
+        }
+    
+        
+    double distance(){
+        return sqrt(x*x+y*y);
+    }
+};
+
+int main(){
+    point p1,p2;
+    int a,b;
+    while(cin>>a>>b){
+        p1.set(a,b); //一對一改變
+        cout<<p1.distance()<<'\n';
+    }
+}
+
